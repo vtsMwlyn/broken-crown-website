d574aea405e236dff932986c2834238268ae93c2
VTS <vannestheo@gmail.com>
2025-06-25 10:09:31 +0700

    resources extraction from figma

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import SectionContainer from "./components/SectionContainer"
 function App() {
   return (
     <>
-      <SectionContainer className="h-screen">
+      <SectionContainer className="h-screen" backgroundImage="url('/page-hero-bg.png')">
         <img src="/logo.svg" className="w-1/2" alt="Brimvahl Crown Logo" />
       </SectionContainer>
-      <SectionContainer className="h-screen">
-        <h1 className="font-bold text-5xl">Characters</h1>
+      <SectionContainer className="h-screen" backgroundImage="url('/characters-section-bg.png')">
+        <h1 className="font-bold text-5xl text-white">Characters</h1>
       </SectionContainer>
-      <SectionContainer className="h-screen">
-        <h1 className="font-bold text-5xl">News and Info</h1>
+      <SectionContainer className="h-screen" backgroundImage="url('/news-and-info-section-bg.png')">
+        <h1 className="font-bold text-5xl text-white">News and Info</h1>
       </SectionContainer>
-      <SectionContainer className="h-screen">
-        <h1 className="font-bold text-5xl">Gameplay Feature</h1>
+      <SectionContainer className="h-screen" backgroundImage="url('/gameplay-feature-section-bg.png')">
+        <h1 className="font-bold text-5xl text-white">Gameplay Feature</h1>
       </SectionContainer>
-      <SectionContainer className="h-screen">
-        <h1 className="font-bold text-5xl">Background</h1>
+      <SectionContainer className="h-screen" backgroundImage="url('/background-section-bg.png')">
+        <h1 className="font-bold text-5xl text-white">Background</h1>
       </SectionContainer>
     </>
   )
--- src/components/SectionContainer.jsx
+++ src/components/SectionContainer.jsx
@@ -1,6 +1,6 @@
-export default function SectionContainer({children, className}) {
+export default function SectionContainer({children, className, backgroundImage}) {
 	return (
-		<div className="w-full flex justify-center">
+		<div className="w-full flex justify-center" style={{background: `${backgroundImage} no-repeat center center / cover`}}>
 			<div className={`w-11/12 xl:w-5/6 flex flex-col justify-center items-center ${className}`}>
 				{children}
 			</div>
