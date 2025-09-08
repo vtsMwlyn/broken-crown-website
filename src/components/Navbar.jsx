b344b8163b64832b68ce5a6ab5c95fd9a78a3e17
asep <radikadybala10@gmail.com>
2025-09-08 14:46:03 +0700

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,73 +1,157 @@
-import { useLocation } from "react-router-dom"
-import { useEffect } from "react";
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
 
 export default function Navbar() {
-	const location = useLocation();
+  const location = useLocation()
+  const [isExpanded, setIsExpanded] = useState(false)
+  const [isAtThePageTop, setIsAtThePageTop] = useState(true)
 
-	const [isExpanded, setIsExpanded] = useState(false);
-	const [isAtThePageTop, setIsAtThePageTop] = useState(true);
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setIsAtThePageTop(true)
 
-	useEffect(() => {
-		if (location.pathname === '/') {
-			setIsAtThePageTop(true);
+      const handleScroll = () => {
+        setIsAtThePageTop(window.scrollY < window.innerHeight / 4)
+      }
 
-			const handleScroll = () => {
-				// Check if the user has scrolled more than 100vh
-				// window.innerHeight is the pixel height of the viewport
-				if (window.scrollY < window.innerHeight / 4) {
-					setIsAtThePageTop(true);
-				} else {
-					setIsAtThePageTop(false);
-				}
-			};
+      window.addEventListener("scroll", handleScroll)
+      return () => window.removeEventListener("scroll", handleScroll)
+    } else {
+      setIsAtThePageTop(false)
+    }
+  }, [location.pathname])
 
-			// Add the event listener when the component mounts
-			window.addEventListener('scroll', handleScroll);
+  return (
+    <nav className="w-screen fixed top-0 z-50 h-[100px]">
+      <img
+        src="/battlements-top.webp"
+        alt="Brimvahl-Crown-Navbar-BG"
+        className="w-screen h-full absolute scale-100 sm:scale-125 md:scale-110 lg:scale-100 origin-top transition-transform duration-500"
+      />
 
-			// IMPORTANT: Clean up the event listener when the component unmounts
-			// This prevents memory leaks and errors.
-			return () => {
-				window.removeEventListener('scroll', handleScroll);
-			};
-		} else {
-			setIsAtThePageTop(false);
-		}
-	}, [location.pathname]);
+      <div className="hidden md:flex justify-between items-center w-11/12 xl:w-5/6 mx-auto relative">
+        <img
+          src="/navbar-logo.webp"
+          alt="Brimvahl-Crown-Navbar-Logo"
+          className={`h-[150px] md:h-[150px] lg:h-[180px] relative top-0 transition ease-in-out duration-1000 ${
+            isAtThePageTop ? "translate-y-[-250px]" : "translate-y-0"
+          }`}
+        />
 
-	return (
-		<nav className="w-full fixed top-0 z-100 flex gap-6 h-[100px]">
-			<img src="/battlements-top.webp" className="w-full h-full absolute" alt="Brimvahl-Crown-Navbar-BG" />
-			<div className="flex w-11/12 xl:w-5/6">
-				<img src="/navbar-logo.webp" className={`h-[220px] relative top-0 transition ease-in-out duration-1000 ${isAtThePageTop ? 'translate-y-[-250px]' : 'translate-y-0'} ml-60`} alt="Brimvahl-Crown-Navbar-Logo" />
-				<div className="relative top-[-20px] flex gap-3 ml-10">
-					<Link to="/" className="relative text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)]">
-						<img src="/navbar-menu-bg-blue.webp" className="w-[160px]" alt="Brimvahl-Crown-Navbar-Menu" />
-						<div className="absolute top-8 text-center w-full">Homepage</div>
-					</Link>
-					<Link to="/characters" className="relative text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)]">
-						<img src="/navbar-menu-bg-blue.webp" className="w-[160px]" alt="Brimvahl-Crown-Navbar-Menu" />
-						<div className="absolute top-8 text-center w-full">Characters</div>
-					</Link>
-					<Link to="/background" className="relative text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)]">
-						<img src="/navbar-menu-bg-blue.webp" className="w-[160px]" alt="Brimvahl-Crown-Navbar-Menu" />
-						<div className="absolute top-8 text-center w-full">Background</div>
-					</Link>
-					<button onClick={() => setIsExpanded(!isExpanded)} className="relative top-[-20px] text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)] flex flex-col items-center">
-						{/* Trigger button */}
-						<img src="/navbar-menu-bg-red.webp" className={`w-[160px] transition ease-in-out duration-500 transform ${isExpanded ? 'translate-y-0' : 'translate-y-[-150px]'}`} alt="Brimvahl-Crown-Navbar-Menu" />
-						<div className={`absolute top-0 text-center w-5/6 transition ease-in-out duration-500 ${isExpanded ? 'translate-y-[185px] scale-0' : 'translate-y-[55px] scale-100'}`}>More</div>
-						<img src="/navbar-more-arrow.webp" className={`absolute transition ease-in-out top-0 duration-500 transform ${isExpanded ? 'translate-y-[220px] rotate-[180deg]' : 'translate-y-[90px] rotate-[0deg]'} left-15 w-[40px]`} alt="Brimvahl-Crown-More-Arrow" />
+        <div className="flex gap-3 ml-10 relative top-[-40px] h-25">
+          <Link to="/" className="relative text-white hover:text-yellow-500">
+            <img src="/navbar-menu-bg-blue.webp" alt="menu" className="w-[160px]" />
+            <div className="absolute top-6 text-center w-full">Homepage</div>
+          </Link>
+          <Link to="/characters" className="relative text-white hover:text-yellow-500">
+            <img src="/navbar-menu-bg-blue.webp" alt="menu" className="w-[160px]" />
+            <div className="absolute top-6 text-center w-full">Characters</div>
+          </Link>
+          <Link to="/background" className="relative text-white hover:text-yellow-500">
+            <img src="/navbar-menu-bg-blue.webp" alt="menu" className="w-[160px]" />
+            <div className="absolute top-6 text-center w-full">Background</div>
+          </Link>
 
-						{/* More menu list */}
-						<div className={`w-full flex flex-col items-center absolute transition ease-in-out top-0 duration-500 transform ${isExpanded ? 'translate-y-[110px] opacity-100' : 'translate-y-[-50px] opacity-0'} gap-6 w-[40px]`}>
-							<Link to="/merch" className="relative w-full text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)]">Merch</Link>
-							<Link to="/fan-arts" className="relative w-full text-white hover:text-yellow-500 hover:drop-shadow-[0px_0px_5px_oklch(79.5%_0.184_86.047)]">Fan Arts</Link>
-						</div>
-					</button>
-				</div>
-			</div>
-		</nav>
-	)
+          <button
+            onClick={() => setIsExpanded(!isExpanded)}
+            className="relative top-[-20px] text-white hover:text-yellow-500 flex flex-col items-center"
+          >
+            <img
+              src="/navbar-menu-bg-red.webp"
+              alt="Navbar-Menu"
+              className={`w-[160px] transition duration-500 ${
+                isExpanded ? "translate-y-0" : "translate-y-[-100px]"
+              }`}
+            />
+            <div
+              className={`absolute top-0 w-5/6 text-center transition duration-500 ${
+                isExpanded ? "translate-y-[185px] scale-0" : "translate-y-[55px] scale-100"
+              }`}
+            >
+              More
+            </div>
+            <img
+              src="/navbar-more-arrow.webp"
+              alt="More-Arrow"
+              className={`absolute transition duration-500 ${
+                isExpanded ? "translate-y-[200px] rotate-180" : "translate-y-[90px] rotate-0"
+              } w-[40px]`}
+            />
+            <div
+              className={`absolute top-0 flex flex-col items-center gap-6 transition duration-500 ${
+                isExpanded ? "translate-y-[110px] opacity-100" : "-translate-y-[50px] opacity-0"
+              }`}
+            >
+              <Link to="/merch" className="text-white hover:text-yellow-500">
+                Merch
+              </Link>
+              <Link to="/fan-arts" className="text-white hover:text-yellow-500">
+                Fan Arts
+              </Link>
+            </div>
+          </button>
+        </div>
+      </div>
+
+      <div className="md:hidden overflow-hidden flex justify-between items-center px-4 sticky top-0">
+        <img
+          src="/navbar-logo.webp"
+          alt="Brimvahl-Crown-Navbar-Logo"
+          className={`h-[100px] sm:h-[150px] relative z-50 top-0 transition ease-in-out duration-1000 ${
+            isAtThePageTop ? "translate-y-[-250px]" : "translate-y-[-67px] sm:translate-y-[-45px]"
+          }`}
+        />
+
+        <button
+          onClick={() => setIsExpanded(!isExpanded)}
+          className="relative flex flex-col items-center text-white hover:text-yellow-500 transition"
+        >
+          <img
+            src="/navbar-menu-bg-red.webp"
+            alt="Navbar Menu"
+            className={`w-[120px] transition-transform duration-500 ${
+              isExpanded ? "translate-y-[-10px]" : "-translate-y-[110px]"
+            }`}
+          />
+          <div
+            className={`absolute top-0 w-5/6 text-center text-lg font-base transition duration-500 ${
+              isExpanded ? "translate-y-[160px] scale-0 opacity-0" : "translate-y-[20px] scale-100 opacity-100"
+            }`}
+          >
+            Menu
+          </div>
+          <img
+            src="/navbar-more-arrow.webp"
+            alt="More Arrow"
+            className={`absolute left-1/2 -translate-x-1/2 w-[40px] transition duration-500 ${
+              isExpanded ? "translate-y-[160px] rotate-180 scale-75" : "translate-y-[60px] rotate-0"
+            }`}
+          />
+          <div
+            className={`absolute top-0 mt-[20px] flex flex-col items-center w-[100px] text-sm font-medium transition duration-500 ${
+              isExpanded ? "translate-y-0 opacity-100" : "-translate-y-[50px] opacity-0 pointer-events-none"
+            }`}
+          >
+            <div className="flex flex-col gap-3 font-base pb-10">
+              <Link to="/" className="text-white hover:text-yellow-500">
+                Homepage
+              </Link>
+              <Link to="/characters" className="text-white hover:text-yellow-500">
+                Characters
+              </Link>
+              <Link to="/background" className="text-white hover:text-yellow-500">
+                Background
+              </Link>
+              <Link to="/merch" className="text-white hover:text-yellow-500">
+                Merch
+              </Link>
+              <Link to="/fan-arts" className="text-white hover:text-yellow-500">
+                Fan Arts
+              </Link>
+            </div>
+          </div>
+        </button>
+      </div>
+    </nav>
+  )
 }
